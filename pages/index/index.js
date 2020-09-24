6256b996b4934c113b5f7bd37ba21e06314b9d75
huangkuiwei <1017969390@qq.com>
2020-09-24 23:29:06 +0800

    首页 banner

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,1 +1,43 @@
-Page({})
+const request = require('../../tools/request')
+
+Page({
+  data: {
+    bannerList: [],
+    buyList: []
+  },
+
+  onLoad() {
+    this.getBannerList()
+    this.getBuyList()
+  },
+
+  /**
+   * 获取轮播图数据
+   */
+  getBannerList() {
+    request
+      .get('tz/banner/list', {
+        type: 'index'
+      })
+      .then((data) => {
+        this.setData({
+          bannerList: data
+        })
+      })
+  },
+
+  /**
+   * 获取购买记录滚动条记录
+   */
+  getBuyList() {
+    request
+      .get('tz/site/goods/dynamic', {
+        type: 0
+      })
+      .then((data) => {
+        this.setData({
+          buyList: data
+        })
+      })
+  }
+})
