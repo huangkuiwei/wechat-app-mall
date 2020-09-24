55587e986ead4d8c989ee152205555b5a9bd5cde
huangkuiwei <1017969390@qq.com>
2020-09-24 22:13:26 +0800

    首页

--- pages/start/start.js
+++ pages/start/start.js
@@ -3,7 +3,7 @@ const request = require('../../tools/request')
 
 Page({
   data: {
-    bannerList: []
+    bannerList: [] // banner 图片列表
   },
 
   onLoad() {
@@ -23,7 +23,40 @@ Page({
           })
         })
     } else {
-      // TODO 跳转到 index 首页
+      this.jumpIndex()
     }
+  },
+
+  /**
+   * 点击 banner 图片
+   * @param {事件对象} event
+   */
+  tapBanner(event) {
+    // 通过 dataset 获取当前 banner 图片的下标
+    const index = event.target.dataset.index
+
+    if (index === this.data.bannerList.length - 1) {
+      // 最后一张 banner 图片，跳转到 index
+      this.jumpIndex()
+    } else {
+      // 其它图片给予提示
+      wx.showToast({
+        title: '左滑进入',
+        icon: 'none'
+      })
+    }
+  },
+
+  /**
+   * 跳转到 index 页面
+   */
+  jumpIndex() {
+    // 设置状态
+    wx.setStorageSync('hasShowStartImg', true)
+
+    // 页面跳转，wx.switchTab 只能跳转发哦 tab 页面
+    wx.switchTab({
+      url: '/pages/index/index'
+    })
   }
 })
